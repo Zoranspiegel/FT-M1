3d9505783e25e5adf1d129faefad352e7cc2b992
Zoranspiegel <zoranbow@gmail.com>
2022-07-22 01:59:32 -0500

    04-EstructuraDeDatos-I/homework done!

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 Definir las funciones recursivas nFactorial y nFibonacci.
@@ -15,9 +15,14 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if (n >= 0 && n <= 1) return 1;
+  else if (n < 0) return "Los números negativos no tienen factorial";
+  return n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
+  if (n > -1 && n < 2) return n;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 }
 
 /*
@@ -30,14 +35,23 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+  this.arr = [];
 }
 
+Queue.prototype.enqueue = function (item) {
+  return this.arr.push(item);
+};
+Queue.prototype.dequeue = function () {
+  return this.arr.shift();
+};
+Queue.prototype.size = function () {
+  return this.arr.length;
+};
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
 module.exports = {
   Queue,
   nFactorial,
-  nFibonacci
+  nFibonacci,
 };
